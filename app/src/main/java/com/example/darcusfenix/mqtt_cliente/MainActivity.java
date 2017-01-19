451065667104d782e522487315201539758963e8
Juan Crisóstomo <juancvfenix@gmail.com>
2017-01-18 19:08:33 -0600

    FUNCIONANDO

--- app/src/main/java/com/example/darcusfenix/mqtt_cliente/MainActivity.java
+++ app/src/main/java/com/example/darcusfenix/mqtt_cliente/MainActivity.java
@@ -3,18 +3,26 @@ package com.example.darcusfenix.mqtt_cliente;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.Toast;
 
 import org.eclipse.paho.android.service.MqttAndroidClient;
 import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+import java.io.UnsupportedEncodingException;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "Main";
-    private MqttAndroidClient mClient;
+    MqttAndroidClient mClient;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
                 public void onSuccess(IMqttToken asyncActionToken) {
                     // We are connected
                     Log.d(TAG, "onSuccess");
+                    suscribirse();
                 }
 
                 @Override
@@ -62,6 +71,41 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onSuccess(IMqttToken asyncActionToken) {
                     // The message was published
+
+
+                    Log.d(TAG, "onSuccess topic");
+
+                    String payload = "the payload desde android";
+                    byte[] encodedPayload = new byte[0];
+                    try {
+                        encodedPayload = payload.getBytes("UTF-8");
+                        MqttMessage message = new MqttMessage(encodedPayload);
+                        message.setRetained(true);
+
+                        mClient.publish("mqtt/demo", message);
+                    } catch (UnsupportedEncodingException | MqttException e) {
+                        e.printStackTrace();
+                    }
+
+                    mClient.setCallback(new MqttCallback() {
+                        @Override
+                        public void connectionLost(Throwable cause) {
+
+                        }
+
+                        @Override
+                        public void messageArrived(String topic, MqttMessage message) throws Exception {
+                            Log.d(TAG, "MENSAJE ENTRANTE");
+                            Log.d(TAG, topic);
+                            Toast.makeText(getApplication(), message.toString(), Toast.LENGTH_SHORT).show();
+                        }
+
+                        @Override
+                        public void deliveryComplete(IMqttDeliveryToken token) {
+
+                        }
+                    });
+
                 }
 
                 @Override
@@ -70,8 +114,10 @@ public class MainActivity extends AppCompatActivity {
                     // The subscription could not be performed, maybe the user was not
                     // authorized to subscribe on the specified topic e.g. using wildcards
 
+                    Log.d(TAG, "error topic");
                 }
             });
+
         } catch (MqttException e) {
             e.printStackTrace();
         }
